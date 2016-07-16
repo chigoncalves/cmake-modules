265b4184fc4565a4955804de96a2679826dceb0e
Edelcides Gonçalves <eatg75@gmail.com>
2016-07-16 19:51:25 +0100

    Improved `FindSN' module.
    * FindSN now exports the version variables.
    
    * FindSN now exports compiler defintions.
    
    * Fixed coding style.
    
    * Removed old crufts.
    
    For more information see the documentation section in `FindSN.cmake'.

--- cmake/modules/FindSN.cmake
+++ cmake/modules/FindSN.cmake
@@ -10,6 +10,18 @@ Upon return it sets the following variables:
 SN_FOUND
   With a ``true`` value if it manages to find the library.
 
+SN_DEFINITIONS
+
+SN_ROOT_DIR
+
+SN_VERSION_STRING
+
+SN_VERSION_MAJOR
+
+SN_VERSION_MINOR
+
+SN_VERSION_PATCH
+
 SN_INCLUDE_DIRS
 
 SN_LIBRARIES
@@ -17,75 +29,120 @@ SN_LIBRARIES
 
 include (SelectLibraryConfigurations)
 include (FindPackageHandleStandardArgs)
-include (CMakePrintHelpers)
 
-function (__sn_find_include_dir)
+function (_sn_find_include_dir)
   find_path (SN_INCLUDE_DIR
-    libsn/sn.h
-    PATH_SUFFIXES startup-notification-1.0 startup-notification-1 startup-notification)
+             libsn/sn.h
+	     HINT ${SN_ROOT_DIR}
+	     ENV SN_ROOT_DIR
+	     PATH_SUFFIXES startup-notification
+	     startup-notification-1
+             startup-notification-1.0)
 
   if (SN_INCLUDE_DIR)
     set (SN_INCLUDE_DIR ${SN_INCLUDE_DIR} PARENT_SCOPE)
   endif ()
 endfunction ()
 
-function (__sn_find_library)
-  set (sn_suffixes 1.0 1)
+function (_sn_find_definitions)
+  if (SN_INCLUDE_DIR)
+    file (STRINGS "${SN_INCLUDE_DIR}/libsn/sn-common.h" _RESULTS REGEX
+          "#ifndef[\t ]+[a-bA-Z0-9_-]+")
+
+    set (SN_DEFINITIONS)
+    foreach (definition ${_RESULTS})
+      string (REPLACE "#ifndef" "" definition "${definition}")
+      string (STRIP "${definition}" definition)
+      string (REGEX MATCH "^_+|_+$" matched "${definition}")
+
+      if (NOT matched)
+	list (APPEND SN_DEFINITIONS "-D${definition}=1")
+      endif ()
+    endforeach ()
+    string (REPLACE ";" " " SN_DEFINITIONS "${SN_DEFINITIONS}")
+    set (SN_DEFINITIONS "${SN_DEFINITIONS}" PARENT_SCOPE)
+  endif ()
+endfunction ()
+
+function (_sn_find_library)
+  set (suffixes 1.0 1)
   set (sn_names startup-notification)
-  foreach (suffix ${sn_suffixes})
+  foreach (suffix ${suffixes})
     list (APPEND sn_names startup-notification-${suffix})
   endforeach ()
 
   find_library (SN_LIBRARY_RELEASE
     ${sn_names}
+
+    HINT ${SN_ROOT_DIR}
+    ENV SN_ROOT_DIR
     PATHS ${CMAKE_LIBRARY_ARCHITECTURE})
 
   if (SN_LIBRARY_RELEASE)
     select_library_configurations (SN)
     set (SN_LIBRARY ${SN_LIBRARY} PARENT_SCOPE)
     set (SN_LIBRARY_RELEASE ${SN_LIBRARY_RELEASE} PARENT_SCOPE)
-    set (SN_LIBRARY_DEBUG ${SN_LIBRARY_DEBUG} PARENT_SCOPE)
+  endif ()
 endfunction ()
 
 
 ##### Entry Point #####
-
-
+set (SN_FOUND)
 set (SN_INCLUDE_DIRS)
 set (SN_LIBRARIES)
-set (SN_VERSION 1.0)
+set (SN_DEFINITIONS)
+# StartupNotification does not export versions via macro, there no
+# way to find it, so we are setting it manually.
+set (SN_VERSION_MAJOR 1)
+set (SN_VERSION_MINOR 0)
+set (SN_VERSION_PATCH 0)
+set (SN_VERSION_STRING "${SN_VERSION_MAJOR}.${SN_VERSION_MINOR}.\
+${SN_VERSION_PATCH}")
 
-if (SN_FIND_COMPONENTS)
-  if (NOT SN_FIND_QUIETLY)
-    message (WARNING "FindSN: Requested a component, but LibSN does not"
-      " have any.")
-  endif ()
+_sn_find_include_dir ()
+if (SN_INCLUDE_DIR)
+  _sn_find_definitions ()
 endif ()
+_sn_find_library ()
 
-if (SN_FIND_VERSION)
-  if (NOT SN_FIND_QUIETLY)
-    message (WARNING "FindSN: Requested `version' but none will be"
-      " inforced.")
-  endif ()
-endif ()
-
-__sn_find_include_dir ()
-__sn_find_library ()
-
-select_library_configurations (SN
-  FOUND_VAR SN_FOUND
-  REQUIRED_VARS SN_INCLUDE_DIR SN_LIBRARY
-)
+find_package_handle_standard_args (SN
+                                   FOUND_VAR SN_FOUND
+				   REQUIRED_VARS
+				   SN_INCLUDE_DIR
+				   SN_LIBRARY
+				   SN_DEFINITIONS
+				   VERSION_VAR
+				   SN_VERSION_STRING)
 
 if (SN_FOUND)
   set (SN_INCLUDE_DIRS ${SN_INCLUDE_DIR})
   set (SN_LIBRARIES ${SN_LIBRARY})
 endif ()
 
-mark_as_advanced (
-  SN_INCLUDE_DIRS
-  SN_INCLUDE_DIR
-  SN_LIBRARY
-  SN_LIBRARIES
-  SN_FOUND
-)
+if (NOT TARGET SN::Library)
+  add_library (SN::LIBRARY UNKNOWN IMPORTED)
+  set_property (TARGET SN::LIBRARY APPEND PROPERTY
+                IMPORTED_CONFIGURATIONS RELEASE)
+
+  set_target_properties (SN::LIBRARY
+                         PROPERTIES
+			 INTERFACE_LOCATION "${SN_LIBRARY}"
+			 INTERFACE_INCLUDE_DIRECTORIES
+			 "${SN_INCLUDE_DIR}")
+
+  set_target_properties (SN::LIBRARY PROPERTIES
+                         IMPORTED_LOCATION_RELEASE
+			 ${SN_LIBRARY_RELEASE})
+endif ()
+
+mark_as_advanced (SN_DEFINITIONS
+                  SN_VERSION_STRING
+		  SN_VERSION_MAJOR
+		  SN_VERSION_MINOR
+		  SN_VERSION_PATCH
+		  SN_FOUND
+		  SN_INCLUDE_DIRS
+		  SN_INCLUDE_DIR
+		  SN_LIBRARIES
+		  SN_LIBRARY_RELEASE
+		  SN_LIBRARY)
