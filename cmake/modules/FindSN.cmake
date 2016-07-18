bcf693a57c8d6ed8bbb1e81dd489cfa183fc976e
Edelcides Gonçalves <eatg75@gmail.com>
2016-07-18 01:54:20 +0100

    Cleaned up FindSN module and forced coding style.
    * Like the other modules in this directory, FindSN
    now also exports libraries in the SN_LIBRARIES in this
    form NAME::Library.

--- cmake/modules/FindSN.cmake
+++ cmake/modules/FindSN.cmake
@@ -14,7 +14,11 @@ job.
 
 .. variable:: SN_INCLUDE_DIRS
 
+   With a list of include directories.
+
 .. variable:: SN_LIBRARIES
+
+   A list of libraries to link your application against.
 
 .. variable:: SN_DEFINITIONS
 
@@ -22,7 +26,22 @@ job.
 
 Variables tha provide hints
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~
-.. variable:: SN_ROOT_DIR
+.. :envvar:`SN_ROOT_DIR`
+
+  Use this variable to provide hints to :filename:`find_{*}` commands,
+  you may pass it to :command:`cmake` or set the environtment variable.
+
+.. code-block:: cmake
+
+   % cmake . -Bbuild -DSN_ROOT_DIR=<extra-path>
+
+   # or
+   % export SN_ROOT_DIR=<extra-path>;
+   % cmake . -Bbuild
+
+   # or
+   % SN_ROOT_DIR=<extra-path> cmake . -Bbuild
+
 
 #]]
 
@@ -39,25 +58,28 @@ include (FindPackageHandleStandardArgs)
 function (_sn_find_include_dir)
   find_path (SN_INCLUDE_DIR
              libsn/sn.h
-	     HINT ${SN_ROOT_DIR}
-	     ENV SN_ROOT_DIR
-	     PATH_SUFFIXES startup-notification
-	     startup-notification-1
-             startup-notification-1.0)
+	     HINT
+	       "${SN_ROOT_DIR}"
+	       ENV SN_ROOT_DIR
+	     PATH_SUFFIXES
+	       startup-notification
+   	       startup-notification-1
+               startup-notification-1.0)
 
   if (SN_INCLUDE_DIR)
-    set (SN_INCLUDE_DIR ${SN_INCLUDE_DIR} PARENT_SCOPE)
+    set (SN_INCLUDE_DIR "${SN_INCLUDE_DIR}" PARENT_SCOPE)
   endif ()
 endfunction ()
 
 function (_sn_find_definitions)
   if (SN_INCLUDE_DIR)
     file (STRINGS "${SN_INCLUDE_DIR}/libsn/sn-common.h" _RESULTS REGEX
-          "#ifndef[\t ]+[a-bA-Z0-9_-]+")
+          "(#ifndef|#ifdef)[\t ]+[a-bA-Z0-9_-]+")
 
     set (SN_DEFINITIONS)
     foreach (definition ${_RESULTS})
       string (REPLACE "#ifndef" "" definition "${definition}")
+      string (REPLACE "#ifdef" "" definition "${definition}")
       string (STRIP "${definition}" definition)
       string (REGEX MATCH "^_+|_+$" matched "${definition}")
 
@@ -66,6 +88,7 @@ function (_sn_find_definitions)
       endif ()
     endforeach ()
     string (REPLACE ";" " " SN_DEFINITIONS "${SN_DEFINITIONS}")
+    list (REMOVE_DUPLICATES SN_DEFINITIONS)
     set (SN_DEFINITIONS "${SN_DEFINITIONS}" PARENT_SCOPE)
   endif ()
 endfunction ()
@@ -78,16 +101,19 @@ function (_sn_find_library)
   endforeach ()
 
   find_library (SN_LIBRARY_RELEASE
-    ${sn_names}
-
-    HINT ${SN_ROOT_DIR}
-    ENV SN_ROOT_DIR
-    PATHS ${CMAKE_LIBRARY_ARCHITECTURE})
+                ${sn_names}
+		HINT
+		  "${SN_ROOT_DIR}"
+		  ENV SN_ROOT_DIR
+		PATHS
+		  /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}
+		  /usr/lib64/${CMAKE_LIBRARY_ARCHITECTURE}
+		  /usr/lib32/${CMAKE_LIBRARY_ARCHITECTURE})
 
   if (SN_LIBRARY_RELEASE)
     select_library_configurations (SN)
-    set (SN_LIBRARY ${SN_LIBRARY} PARENT_SCOPE)
-    set (SN_LIBRARY_RELEASE ${SN_LIBRARY_RELEASE} PARENT_SCOPE)
+    set (SN_LIBRARY "${SN_LIBRARY}" PARENT_SCOPE)
+    set (SN_LIBRARY_RELEASE "${SN_LIBRARY_RELEASE}" PARENT_SCOPE)
   endif ()
 endfunction ()
 
@@ -106,36 +132,30 @@ endif ()
 _sn_find_library ()
 
 find_package_handle_standard_args (SN
-                                   FOUND_VAR SN_FOUND
 				   REQUIRED_VARS
-				   SN_INCLUDE_DIR
-				   SN_LIBRARY
-				   SN_DEFINITIONS
-				   VERSION_VAR
-				   SN_VERSION_STRING)
-
+ 				     SN_INCLUDE_DIR
+				     SN_LIBRARY
+				     SN_DEFINITIONS)
 if (SN_FOUND)
   set (SN_INCLUDE_DIRS ${SN_INCLUDE_DIR})
-  set (SN_LIBRARIES ${SN_LIBRARY})
-endif ()
 
-if (NOT TARGET SN::Library)
-  add_library (SN::LIBRARY UNKNOWN IMPORTED)
-  set_property (TARGET SN::LIBRARY APPEND PROPERTY
-                IMPORTED_CONFIGURATIONS RELEASE)
+  if (NOT TARGET SN::Library)
+    add_library (SN::Library UNKNOWN IMPORTED)
+    set_property (TARGET SN::Library APPEND
+                  PROPERTY
+                    IMPORTED_CONFIGURATIONS RELEASE)
 
-  set_target_properties (SN::LIBRARY
+    set_target_properties (SN::Library
                          PROPERTIES
-			 INTERFACE_LOCATION "${SN_LIBRARY}"
-			 INTERFACE_INCLUDE_DIRECTORIES
-			 "${SN_INCLUDE_DIR}")
-
-  set_target_properties (SN::LIBRARY PROPERTIES
-                         IMPORTED_LOCATION_RELEASE
-			 ${SN_LIBRARY_RELEASE})
+			   INTERFACE_INCLUDE_DIRECTORIES
+			     "${SN_INCLUDE_DIR}"
+			   INTERFACE_LOCATION "${SN_LIBRARY}"
+			   IMPORTED_LOCATION_RELEASE
+			     "${SN_LIBRARY_RELEASE}")
+    set (SN_LIBRARIES SN::Library)
+  endif ()
 endif ()
 
 mark_as_advanced (SN_INCLUDE_DIR
-		  SN_LIBRARY_DIR
-		  SN_LIBRARY_RELEASE
-		  SN_LIBRARY)
+		  SN_LIBRARY
+		  SN_LIBRARY_RELEASE)
